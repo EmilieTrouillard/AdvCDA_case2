12cb4cd9979e2b2f06316b7937155b27f6a7bd1f
EmilieTrouillard <s172727@student.dtu.dk>
2018-12-06 23:26:40 +0100

    cleaning + changes in the freq itemsets to fit the other code

--- Rulegen.py
+++ Rulegen.py
@@ -6,35 +6,6 @@ Created on Wed Dec  5 14:34:34 2018
 """
 import numpy as np
 import itertools
-def findsubsets(S,m):
-    return list(itertools.combinations(S, m))
-def tempfix(f,key):
-    r=np.array(f)
-    a=np.zeros(shape=(len(f),key))
-    r1=np.zeros(shape=(len(f),1))
-    b=[]
-    for k in range(np.shape(a)[0]):
-        r1[k]=r[k][1]
-        b.append(r[k][0])
-        for i in range(np.shape(a)[1]):
-            a[k,i]=r[k,0][i]
-    
-    return(a,r1,b)
-def ConfGet(superset,subset,superitem,subitem,occsuper,occsub):
-    #Antecedent +consequent, antecedent, frequent itemset for superset, freq items for ant. ,
-    #occurence for ant + cons,  
-    antsub=occsub[subitem.index(subset)][0]
-    conssuper=occsuper[superitem.index(superset)][0]
-    conf=conssuper/float(antsub)
-    return conf
-
-def ConfGet(superset,subset,superitem,subitem,occsuper,occsub):
-    #Antecedent +consequent, antecedent, frequent itemset for superset, freq items for ant. ,
-    #occurence for ant + cons,  
-    antsub=occsub[subitem.index(subset)][0]
-    conssuper=occsuper[superitem.index(superset)][0]
-    conf=conssuper/float(antsub)
-    return conf
 
 def confget(ante,conseq):
     #Antecedent +consequent, antecedent, frequent itemset for superset, freq items for ant. ,
@@ -45,47 +16,24 @@ def confget(ante,conseq):
     conf=conseqsup/float(antsup)
     return conf
 
-def ConfGet1(superset,subset,superitem,occsuper):
-    
-    antsub=o[0].loc[subset]
-    conssuper=occsuper[superitem.index(superset)][0]
-    conf=conssuper/float(antsub)
-    return conf
-
-def updateitems(ants,conseq,count):
-    conslist=[]
-    antlist=[]
-    if type(conseq[0]==int):
-        for k in range(len(ants)):
-            for s in range(k+1,len(ants)):
-                if len(np.intersect1d(list(ants[k]),list(ants[s])))==len(ants[k])-1:
-                    newcons= (conseq[k], np.setdiff1d(ants[k],ants[s])[0])
-                    newant=tuple(np.intersect1d(ants[k],ants[s]),*newcons)
-                    antlist.append(newant)
-                    conslist.append(newcons)    
-    else:
-        for k in range(len(ants)):
-            for s in range(k+1,len(ants)):
-                if len(np.intersect1d(list(ants[k]),list(ants[s])))==len(ants[k])-1:
-                    
-                    newcons= (*conseq[k], np.setdiff1d(ants[k],ants[s])[0])
-                    newant=tuple(np.intersect1d(ants[k],ants[s]),*newcons)
-                    antlist.append(newant)
-                    conslist.append(newcons)
-    return (antlist,conslist)
-                    
-
 #%%
 import pandas as pd
 from itertools import combinations, groupby
 from collections import Counter
 
-orders_file = 'orders.csv'
-products_file = 'products.csv'
-prior_file = 'order_products__prior.csv'
-train_file = 'order_products__train.csv'
-aisles_file = 'aisles.csv'
-departments_file = 'departments.csv'
+#orders_file = 'orders.csv'
+#products_file = 'products.csv'
+#prior_file = 'order_products__prior.csv'
+#train_file = 'order_products__train.csv'
+#aisles_file = 'aisles.csv'
+#departments_file = 'departments.csv'
+orders_file = 'instacart_2017_05_01/orders.csv'
+products_file = 'instacart_2017_05_01/products.csv'
+prior_file = 'instacart_2017_05_01/order_products__prior.csv'
+train_file = 'instacart_2017_05_01/order_products__train.csv'
+aisles_file = 'instacart_2017_05_01/aisles.csv'
+departments_file = 'instacart_2017_05_01/departments.csv'
+
 
 #%%
 df_orders = pd.read_csv(orders_file, engine='python')
@@ -144,7 +92,7 @@ aisle_products['frequency'] = aisle_products.apply(frequency, axis=1)
 #%%
 frequent_products = aisle_products[aisle_products['frequency']>= SUPPORT_THRESHOLD]
 #list_frequent_items = [set([item]) for item in frequent_products.index]
-list_frequent_items = [[i] for i in frequent_products.index]
+dict_frequent_items = {(i,): aisle_products.loc[i, 'occurences'] for i in frequent_products.index}
 
 #%%
 order_array = np.array(aisle_ordersproducts, dtype=object)
@@ -175,13 +123,12 @@ def get_itemsets(order_item, candidates,k):
 #%%
 def get_all_frequent_itemsets(order_array, frequent_items):
     flag = True
-    frequent_itemsets_k = frequent_items
+    frequent_itemsets_k = list(frequent_items.keys())
     frequent_itemsets_all = {1: frequent_items}
-    frequent_orders = np.array([item for item in order_array if [item[1]] in frequent_items])
+    frequent_orders = np.array([item for item in order_array if (item[1],) in frequent_itemsets_k])
     k = 1
     while flag:
         k += 1
-        print(k)
         candidates = Counter(get_candidates_subsets(frequent_itemsets_k))
         C = Counter(get_itemsets(frequent_orders, candidates,k))
         C_frequent = C.copy()
@@ -192,96 +139,42 @@ def get_all_frequent_itemsets(order_array, frequent_items):
         if len(frequent_itemsets_k) == 0:
             flag = False
         else:
-            frequent_itemsets_all[k] = [(itemset, occurence) for itemset, occurence in C_frequent.items()]
+            frequent_itemsets_all[k] = {itemset: occurence for itemset, occurence in C_frequent.items()}
     return frequent_itemsets_all
 
 #%%
-F = get_all_frequent_itemsets(order_array, list_frequent_items)
-
-
+F = get_all_frequent_itemsets(order_array, dict_frequent_items)
 
 #%%
-maxrulelen=len(F)
-r2=np.array(F[2])
-r3=np.array(F[3])
-r4=np.array(F[4])
-#%%
-
-#%%
-#r1,o1,b1=tempfix(F[1],1)
-b1=F[1]
-b1=[tuple(l) for l in b1]
-
-r2,o2,b2=tempfix(F[2],2)
-r3,o3,b3=tempfix(F[3],3)
-r4,o4,b4=tempfix(F[4],4)
-o=[aisle_products['occurences'],o2,o3,o4]
-b=[b1,b2,b3,b4]
-threshold=0.25      
-#%%
-    
-
-
-#%%   
- 
-thresh=0.05
-rulelist=[]
-        #%%
-for itemset,occ in F[4].items():
-    cand=list(itemset)
-    for conseq in itemset:
-        antece=list(itemset)
-        antece.remove(conseq)
-        antece=tuple(antece)
-        confi=confget(antece,tuple(itemset))
-        if confi<thresh:
-            cand.remove(conseq)
-        else:
-            rulelist.append((antece,(conseq),confi))
-
-    pairs=findsubsets(cand,2)
-    cand=pairs
-    
-    for conseq in pairs:
-        antece=list(itemset)
-        antece=[x for x in antece if x not in conseq]
-        antece=tuple(antece)
-        confi=confget(antece,itemset)
-        if confi<thresh:
-            cand.remove(conseq)
-        else:
-            rulelist.append((antece,(conseq),confi))
-                #print(antece,conseq)
-#%%effiecient apriori:
-from mlxtend.frequent_patterns import apriori
-apriori(aisle_ordersproducts, min_support=0.6)
+##%%effiecient apriori:
+#from mlxtend.frequent_patterns import apriori
+#apriori(aisle_ordersproducts, min_support=0.6)
 
 
  #%%
-#def update(items,consequents,count)
-maxrule=4
-rulelist=[]                
-for k, dic in F.items():
-    for itemset, occ in dic.items():
-        cand=[(item,) for item in itemset]
-        subsets = cand
-        i = 1
-        while len(cand) > 0 and i < k:
-            for conseq in subsets:
-                antece=[x for x in itemset if x not in conseq]
-                antece=tuple(antece)
-                confi=confget(antece,itemset)
-                if confi<thresh:
-                    cand.remove(conseq)
-                else:
-                    rulelist.append((antece,(conseq,),confi))
-            
-            subsets = list(get_candidates_subsets(cand))
-            cand = subsets
-            i +=1
-      
-
-
+def rule_generation(F, thresh):
+    rulelist=[]                
+    for k, dic in F.items():
+        for itemset in dic.keys():
+            cand=[(item,) for item in itemset]
+            subsets = cand
+            i = 1
+            while len(cand) > 0 and i < k:
+                for conseq in subsets:
+                    antece=[x for x in itemset if x not in conseq]
+                    antece=tuple(antece)
+                    confi=confget(antece,itemset)
+                    if confi<thresh:
+                        cand.remove(conseq)
+                    else:
+                        rulelist.append((antece,conseq,confi))
+                
+                subsets = list(get_candidates_subsets(cand))
+                cand = subsets
+                i +=1
+    return rulelist
+          
+rules = rule_generation(F, 0.1)
 
 
 
